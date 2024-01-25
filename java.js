6a3be7cb40cf9b94d905aa23e61af9d355a02c4b
tabish378 <156536322+tabish378@users.noreply.github.com>
2024-01-25 09:56:12 -0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+// Area of Rectangle:
+let length=25;
+let width=5;
+
+let area=(length*width);
+console.log(area);
+
+// Ask User Name and Alert Message:
+let userName=prompt("Your Name")
+alert("I'm syed tabish ali; Hope you all are having a great Day! I am Working on a Assignment that connects to my website")
+console.log(userName);
